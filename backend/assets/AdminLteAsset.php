833ad55b2d4a70c4f1080eee6d6f7d6626dfcf8d
sunxiaozhi <1129535445@qq.com>
2018-03-06 14:19:55 +0800

    修改文件的引用

--- backend/assets/AdminLteAsset.php
+++ backend/assets/AdminLteAsset.php
@@ -20,7 +20,7 @@ class AdminLteAsset extends AssetBundle
         'admin_lte/dist/css/skins/_all-skins.min.css',
     ];
     public $js = [
-        'admin_lte/components/jquery/jquery.min.js',
+        //'admin_lte/components/jquery/jquery.min.js',
         'admin_lte/components/bootstrap/js/bootstrap.min.js',
         'admin_lte/plugins/iCheck/icheck.min.js',
         'admin_lte/components/fastclick/lib/fastclick.js',
@@ -34,7 +34,7 @@ class AdminLteAsset extends AssetBundle
         'admin_lte/dist/js/demo.js',
     ];
     public $depends = [
-        //'yii\web\YiiAsset',
+        'yii\web\YiiAsset',
         //'yii\bootstrap\BootstrapAsset',
     ];
 }
