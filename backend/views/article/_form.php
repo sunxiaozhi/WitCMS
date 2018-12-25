70c1bb90fe40269688ffd4c7cb79e802806e75b9
sunxiaozhi <1129535445@qq.com>
2018-12-25 08:59:34 +0800

    修改编辑器

--- backend/views/article/_form.php
+++ backend/views/article/_form.php
@@ -45,21 +45,7 @@ $preview[] = Html::img(Url::toImage($model->thumb), ['class' => 'file-preview-im
                     ]
                 ]); ?>
 
-                <?= $form->field($model, 'content')->widget(FroalaEditorWidget::className(), [
-                    'name' => 'body',
-                    'clientOptions' => [
-                        'toolbarInline' => false,
-                        'height' => 600,
-                        'theme' => 'gray',//optional: dark, red, gray, royal
-                        'language' => 'zh_cn',
-                        'toolbarButtons' => ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', '|', 'color', 'emoticons', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', '|', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'quote', 'insertHR', 'undo', 'redo', 'clearFormatting', 'selectAll', 'html'],
-                        'imageUploadParam' => 'file',
-                        'imageUploadURL' => \yii\helpers\Url::to(['img-upload/asyncphoto/'])
-                    ],
-                    'clientPlugins' => ['align', 'char_counter', 'code_beautifier', 'code_view', 'colors', 'draggable', 'emoticons', 'entities', 'file', 'font_family', 'font_size', 'forms', 'fullscreen', 'help', 'image', 'image_manager', 'inline_style', 'line_breaker', 'link', 'lists', 'paragraph_format', 'paragraph_style', 'print', /*'quick_insert',*/
-                        'quote', 'save', 'special_characters', 'table', 'url', 'video', 'word_paste',
-                    ]
-                ]) ?>
+                <?= $form->field($model, 'content')->widget(\crazydb\ueditor\UEditor::className()) ?>
             </div>
             <div class="col-md-4">
                 <div class="box">
