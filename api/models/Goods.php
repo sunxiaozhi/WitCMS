54b95af8d227ae685f837630718a0bbd177c1246
1129535445@qq.com <zhi8023nan1314>
2019-05-30 21:13:41 +0800

    逐步添加api

---
+++ api/models/Goods.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace api\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "goods".
+ *
+ * @property string $id id
+ * @property string $name 商品名称
+ * @property string $price 商品价格
+ */
+class Goods extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'goods';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['price'], 'number'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+        ];
+    }
+}
