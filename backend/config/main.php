2c79d4a42514d9e05f5ee8cd19f2b2bffcf92193
sunxiaozhi <1129535445@qq.com>
2019-05-31 15:53:37 +0800

    后台开启伪静态

--- backend/config/main.php
+++ backend/config/main.php
@@ -50,14 +50,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
             /*'itemTable' => 'auth_item',
