f9288c60c7af93b93349c06177813335f078bf0d
sunxiaozhi <1129535445@qq.com>
2019-10-28 14:41:26 +0800

    格式化代码

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
-Yii::$container->set('yii\data\Pagination', ['defaultPageSize' => 15]);
-Yii::$container->set('yii\grid\ActionColumn', ['buttonOptions' => ['class' => 'btn btn-default btn-xs']]);
+Yii::$container->set('yii\data\Pagination', [
+    'defaultPageSize' => 15
+]);
+Yii::$container->set('yii\grid\ActionColumn', [
+    'buttonOptions' => [
+        'class' => 'btn btn-default btn-xs'
+    ]
+]);
 Yii::$container->set('yii\grid\GridView', [
     'tableOptions' => ['class' => 'table table-bordered table-hover table-responsive'],
     'layout' => "{items}\n{summary}\n{pager}",
